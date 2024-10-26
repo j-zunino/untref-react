b81772dbae299e91c3c360e326f3d4aabbbabe1a
j-zunino <zuninojuanbautista@gmail.com>
2024-10-26 19:32:58 -0300

    clase 02: add styles

--- clase-02/src/components/Caninoinfo.jsx
+++ clase-02/src/components/Caninoinfo.jsx
@@ -1,11 +1,16 @@
-const Caninoinfo = (canino) => {
+import './Caninoinfo.css';
+
+const Caninoinfo = ({ canino }) => {
     return (
-        <div>
-            <h2>Nombre: {canino.nombre}</h2>
-            <p>Edad: {canino.edad}</p>
-            <p>Sexo: {canino.sexo}</p>
-            <p>Raza: {canino.raza}</p>
-            <p>Tamaño: {canino.tamaño}</p>
+        <div className="card">
+            <img src={canino.image} />
+            <div>
+                <h2>{canino.nombre}</h2>
+                <p>Edad: {canino.edad} Años humanos</p>
+                <p>Sexo: {canino.sexo}</p>
+                <p>Raza: {canino.raza}</p>
+                <p>Tamaño: {canino.tamaño}</p>
+            </div>
         </div>
     );
 };
--- clase-02/src/components/Veterinaria.jsx
+++ clase-02/src/components/Veterinaria.jsx
@@ -1,45 +1,51 @@
+import './Veterinaria.css';
 import Caninoinfo from './Caninoinfo.jsx';
 
 const Caninos = [
     {
         id: 1,
-        nombre: 'Firulais',
-        edad: 3 + ' Años Humanos',
-        sexo: 'Macho',
+        nombre: 'firulais',
+        edad: 3,
+        sexo: 'macho',
         raza: 'pastor aleman',
-        tamaño: 'grande'
+        tamaño: 'grande',
+        image: 'https://t2.ea.ltmcdn.com/es/posts/3/2/9/curiosidades_del_pastor_aleman_23923_600_square.jpg'
     },
     {
         id: 2,
-        nombre: 'Maya',
-        edad: 4 + ' Años Humanos',
-        sexo: 'Hembra',
+        nombre: 'maya',
+        edad: 4,
+        sexo: 'hembra',
         raza: 'pug',
-        tamaño: 'chico'
+        tamaño: 'chico',
+        image: 'https://img.freepik.com/fotos-premium/perro-pug-sienta-suelo-frente-gabinete-madera_1011888-27218.jpg?semt=ais_hybrid'
     },
     {
         id: 3,
-        nombre: 'Pipo',
-        edad: 5 + ' Años Humanos',
-        sexo: 'Macho',
+        nombre: 'pipo',
+        edad: 5,
+        sexo: 'macho',
         raza: 'pastor',
-        tamaño: 'mediano'
+        tamaño: 'mediano',
+        image: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-08australian20shepherd20dog2028129_0.jpg?itok=8X9dpMuC'
     },
     {
         id: 4,
         nombre: 'pacha',
-        edad: 4 + ' Años Humanos',
-        sexo: 'Hembra',
+        edad: 4,
+        sexo: 'hembra',
         raza: 'pastor',
-        tamaño: 'chico'
+        tamaño: 'chico',
+        image: 'https://cdn.shopify.com/s/files/1/0012/9469/6494/files/Pastor_Belga_Groenendael_600x600.png?v=1698663434'
     },
     {
         id: 5,
-        nombre: 'Tara',
-        edad: 8 + ' Años Humanos',
-        sexo: 'Hembra',
-        raza: 'Pitbull',
-        tamaño: 'grande'
+        nombre: 'tara',
+        edad: 8,
+        sexo: 'hembra',
+        raza: 'pitbull',
+        tamaño: 'grande',
+        image: 'https://t2.uc.ltmcdn.com/es/posts/9/8/4/como_es_el_temperamento_del_american_pitbull_terrier_51489_600_square.jpg'
     }
 ];
 
@@ -47,13 +53,15 @@ export default function Veterinaria() {
     return (
         <div>
             <h1>Clienes caninos de Veterinaria</h1>
-            <ul>
-                {Caninos.map((canino) => (
-                    <li key={canino.id}>
-                        <Caninoinfo canino={canino} />
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <ul className="cards">
+                    {Caninos.map((canino) => (
+                        <li key={canino.id}>
+                            <Caninoinfo canino={canino} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
